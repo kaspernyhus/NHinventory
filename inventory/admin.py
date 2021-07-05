69ca1413161bedbfa58dd079c538f65546e964d3
Kasper Nyhus <kanyhus@gmail.com>
2021-07-05 16:48:39 +0200

    placement

--- inventory/admin.py
+++ inventory/admin.py
@@ -6,9 +6,12 @@ class PartAdmin(admin.ModelAdmin):
   list_display = ('name', 'description', 'value', 'unit', 'type', 'subtype', 'stock')
 
 
+class LocationAdmin(admin.ModelAdmin):
+  list_display = ('container', 'placement')
+
 admin.site.register(Part, PartAdmin)
 admin.site.register(PartType)
 admin.site.register(Subtype)
 admin.site.register(Unit)
-admin.site.register(Location)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(Placement)
